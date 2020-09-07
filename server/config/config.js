0469cf8cce114caf84cd75796cec3002185eb775
maortega-itti <marcos.ortega@itti.digital>
2020-09-07 16:36:51 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb+srv://marcosOrtega180:5eFCZTEVFig8zWb@cluster0.ez4vd.mongodb.net/cafe'
+    urlDB= process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
