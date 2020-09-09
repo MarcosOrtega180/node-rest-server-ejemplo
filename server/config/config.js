add79832f98172ec0074fc65f8de581b3d7965b2
maortega-itti <marcos.ortega@itti.digital>
2020-09-09 10:55:11 -0400

    Integración de Google Sign-in

--- server/config/config.js
+++ server/config/config.js
@@ -18,11 +18,12 @@ process.env.expiresIn = 60 * 60 * 24 * 30
 //===============
 // Seed de autenticación
 //===============
-process.env.SEED = process.env.SEED||'este-es-el-seed-desarrollo'
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
 
 //===============
 // Data Base
 //===============
+
 
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
@@ -31,4 +32,8 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 
+// ===============
+// google client id
+//===============
+process.env.CLIENT_ID = process.env.CLIENT_ID || '940899324344-72hiq0te30bq09mt4ijfnbpgnbke4238.apps.googleusercontent.com';
 process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 require('./config/config');
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const bodyParser = require('body-parser')
-
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 //parse aplication/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-// app.use(require('./routes/usuario'));
+// habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../server/public')));
+
 //reemplazamos el codigo anterior por el siguiente para importar todoas las rutas de una vez
 //configuración global de rutas
 app.use(require('./routes/index'));
@@ -24,5 +26,5 @@ mongoose.connect(process.env.URLDB, {
 })
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando en el puerto 3000');
+    console.log(`Escuchando en el puerto ${process.env.PORT}`);
 })
